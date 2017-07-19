92aac3191f5cda84a6e003f4bc381b5008b3d18f
Yohany Flores Suarez <yohanyflores@gmail.com>
2017-07-18 23:54:05 -0400

    Creando Dockerfile con el soporte minimo.

--- README.md
+++ README.md
@@ -1,2 +1,26 @@
 # gearman
-Imagen docker para gearman
+Imagen docker para gearman:
+
+* Soporta libsqlite3, libmysql, libdrizzle, libmemcached
+* Soporta ssl con openssl.
+
+* CPP Flags:                  -fvisibility=hidden
+* LIBS:                      
+* LDFLAGS Flags:             
+* Assertions enabled:        no
+* Debug enabled:             no
+* Warnings as failure:       no
+* Building with hiredis      no
+* Building with libsqlite3   yes
+* Building with libdrizzle   yes
+* Building with libmemcached yes
+* Building with libpq        no
+* Building with tokyocabinet no
+* Building with libmysql     yes
+* SSL enabled:               yes
+* wolfssl found:             no
+* openssl found:             yes
+* make -j:                   no
+* VCS checkout:              no
+* sphinx-build:              :
+
---
+++ docker-entrypoint.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+set -e
+
+if [ "${1:0:1}" = '-' ]; then
+	set -- gearmand "$@"
+fi
+
+if [ "$1" = 'gearmand' ]; then
+
+	if [ ! -z $GEARMAN_SQLITE_FILE ]; then
+		set -- "$@" \
+		"--queue-type=libsqlite3" \
+		"--libsqlite3-db=$GEARMAN_SQLITE_FILE"
+	fi
+
+	if [ ! -z $GEARMAN_MEMCACHE_HOST ]; then
+  		set -- "$@" \
+  		    "--queue-type=libmemcached" \
+  		    "--libmemcached-servers=$GEARMAN_MEMCACHE_HOST"
+	fi
+
+	if [ ! -z $GEARMAN_MYSQL_HOST ]; then
+		set -- "$@" "--queue-type=mysql" \
+			"--mysql-host=$GEARMAN_MYSQL_HOST" \
+			"--mysql-port=${GEARMAN_MYSQL_PORT:-3306}" \
+			"--mysql-user=${GEARMAN_MYSQL_USER:-gearman}" \
+			"--mysql-password=${GEARMAN_MYSQL_PASSWORD:-gearman}" \
+			"--mysql-db=${GEARMAN_MYSQL_DB:-gearman}" \
+			"--mysql-table=${GEARMAN_MYSQL_TABLE:-gearman_queue}"
+	fi
+
+	echo "Starting gearman job server with params: $@"
+
+	exec gosu gearman "$@"
+fi
+
+exec "$@"
